7c7cd181287dce26241773a1578a5615136ae74a
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-09-16 18:19:13 +0900

    feat: index.php

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Test</title>
+</head>
+<body>
+    <h1><?php echo "Hello PHP World!"; ?></h1>
+</body>
+</html>
