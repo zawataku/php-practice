25742cc29bb740c779a4991a0a6f3defd889aff1
zawataku <c1254976@st.kanazawa-it.ac.jp>
2025-10-03 01:22:26 +0900

    update: /hello

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<'EOF'
+<html>
+    <head>
+        <title>Laravel Test</title>
+        <style>
+            body {
+                font-size: 16pt;
+                color: #999;
+            }
+            h1 {
+                font-size: 100pt;
+                text-align: right;
+                color: #eee;
+                margin: -40px 0px -50px 0px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hello Laravel</h1>
+        <p>This is Laravel Sample Page</p>
+    </body>
+</html>
+EOF;
+
+Route::get('/hello', function () use ($html) {
+    return $html;
+});
